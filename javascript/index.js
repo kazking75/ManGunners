327aa1d0e391fa9a3b08b2d8d003217b67d96833
kazking75 <82008598+kazking75@users.noreply.github.com>
2021-04-30 17:48:35 +0900

    Add files via upload

---
+++ javascript/index.js
@@ -0,0 +1,12 @@
+$(".fv").slick({
+  autoplay:true,
+  arrows: false,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    speed: 1500,
+    fade: true,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    pauseOnDotsHover: false,
+});
